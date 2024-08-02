bb290aa482c922dbb837a1c55f5531438fcf9f14
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2024-08-02 10:37:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ passport.use("local",
 passport.use("google",new GoogleStrategy({
   clientID:process.env.GOOGLE_CLIENT_ID,
   clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL:"http://localhost:3000/auth/google/secrets",
+  callbackURL:"http://localhost:3000/auth/google/secrets",  //sensitive url
   userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
 },
 
